72630fa7d8529400f61099dad1fe9050cf31ec82
carmonaAP <rgacarmona@gmail.com>
2025-06-25 20:37:35 -0300

    Add files via upload

---
+++ components/navbar.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity} from "react-native";
+
+
+const Navbar = ({title, onLeftPress, onRightPress }) => {
+    return (
+        <View style={styles.navbar}>
+            <TouchableOpacity onPress={onLeftPress}>
+                <Text style={styles.navbarButton}>Voltar</Text>
+            </TouchableOpacity>
+            <Text style={styles.navbarTitle}>{title}</Text>
+            <TouchableOpacity onPress={onRightPress}>
+                <Text style={styles.navbarButton}>Avançar</Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+//folha de estilo 
+const styles = StyleSheet.create({
+    navbar: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alighItems: 'center',
+        backgroundColor: '#f0f0f0',
+        padding: 10,
+    },
+
+    navbarButton: {
+        color: 'blue',
+        fontSize: 16,
+    },
+
+    navbarTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+});
+
+export default Navbar;
+
